c7e22d5a73348f1ba6770104683929ee272f2139
alishalawani <alishalawani@gmail.com>
2020-11-29 21:28:51 -0600

    made the text in the welcome page navigate the user to the record page

--- app/screens/Welcome.js
+++ app/screens/Welcome.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Image, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
 
-function Welcome(props) {
-	let url = 'http://localhost:8000/';
-	let uri = '../app/assets/muse.aiff';
-    let uriParts = uri.split('.');
-	let fileType = uriParts[uriParts.length - 1];
-    let formData = new FormData();
+function Welcome({navigation}) {
+	function upload() {
+		let url = 'http://localhost:8000/';
+		let uri = '../app/assets/muse.aiff';
+		let uriParts = uri.split('.');
+		let fileType = uriParts[uriParts.length - 1];
+		let formData = new FormData();
 		formData.append('recording', {
 			uri,
 			name: `sound.${fileType}`,
 			type: `audio/x-${fileType}`,
 		});
-		formData.append('title','a title')
+		formData.append('title', 'a title');
 		formData.append('subject', 'a subject');
-		console.log(formData)
 
 		let options = {
 			method: 'POST',
@@ -24,14 +24,26 @@ function Welcome(props) {
 				'Content-Type': 'multipart/form-data',
 			},
 		};
-        fetch(url, options).then((res)=>res.json()).then((res)=>{
-			console.log(res)
-		})
-    return (
-        <View>
-            
-        </View>
-    );
+
+		fetch(url, options)
+			.then((res) => res.json())
+			.then((res) => {
+				console.log(res);
+			});
+	}
+
+	return <View>
+		<View onStartShouldSetResponder={
+			()=>{
+				navigation.navigate('RecordView', {name: 'RecordView'})
+			}
+		}>
+			<Text>
+				Make a recording
+			</Text>
+
+		</View>
+	</View>;
 }
 
 export default Welcome;
