27a560160389fe1815f7e2cc576bb5aaacd32bfa
alishalawani <alishalawani@gmail.com>
2020-11-29 21:27:24 -0600

    added a navigation stack

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import Welcome from './app/screens/Welcome';
 import LoginView from './app/screens/LoginView';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import RecordView from './app/screens/RecordView';
 
 const Stack = createStackNavigator();
 export default function App() {
@@ -19,6 +20,11 @@ export default function App() {
 					options={{ title: 'Welcome' }}
 				/>
 				<Stack.Screen name='Login' component={LoginView} />
+				<Stack.Screen
+					name='RecordView'
+					component={RecordView}
+					options={{ title: 'RecordView' }}
+				/>
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
