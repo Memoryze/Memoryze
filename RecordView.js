9526db278781df24577205f40365b8e2a62e9a49
alishalawani <alishalawani@gmail.com>
2020-12-06 21:46:46 -0600

    removed unwanted code

--- RecordView.js
+++ RecordView.js
@@ -14,9 +14,7 @@ function RecordView({ navigate }) {
 	const [fileName, setFilename] = useState('record.mp4');
 	
 	const [player, setPlayer] = useState( new Player);
-	if (player) {
-		player
-	}
+
 	const [recorder, setRecorder] = useState(
 		new Recorder(fileName, {
 			bitrate: 256000,
@@ -29,103 +27,6 @@ function RecordView({ navigate }) {
 		recordButton: recorder && recorder.isRecording ? 'Stop' : 'Record',
 	});
 
-	async function requestRecordPermission() {
-		try {
-			const permissionGranted = await PermissionsAndroid.request(
-				PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
-				{
-					title: 'Microphone Permission',
-					message: 'Memoryze needs access to your microphone for recording',
-					buttonNeutral: 'Ask Me Later',
-					buttonNegative: 'Cancel',
-					buttonPositive: 'OK',
-				}
-			);
-
-			if (permissionGranted === PermissionsAndroid.RESULTS.GRANTED) {
-				return true;
-			} else {
-				return false;
-			}
-		} catch (error) {
-			console.error(error);
-			return false;
-		}
-	}
-
-	const activateMicrophonePermissionRequest = () => {
-		let recordRequest =
-			Platform.OS === 'android'
-				? requestRecordPermission()
-				: new Promise((resolve, reject) => resolve(true));
-
-		recordRequest.then((permitted) => {
-			if (!permitted) {
-				setMicPermissionStatus('denied');
-			}
-		});
-	};
-
-	function playerReload() {
-		if (player) {
-			player.destroy();
-		}
-
-		setPlayer(
-			new Player(fileName, { autoDestroy: false }).prepare((error) => {
-				//this function is to preload the file so it plays immediately it is played
-				if (error) {
-					console.log('reload player error');
-					console.log(error);
-				} else {
-					player.looping = false; //come back to this later
-				}
-
-				//make necessary state updates
-			})
-		);
-
-		//perform operations when the player is paused and ended
-	}
-
-	function recorderReload() {
-		if (recorder) {
-			recorder.destroy();
-		}
-
-		setRecorder(
-			new Recorder(fileName, {
-				bitrate: 256000,
-				quality: 'max',
-			})
-		);
-
-		//make necessary state updates
-	}
-	const playOrPause = () => {
-		player.playPause((error, paused) => {
-			if (err) {
-				setErrorMessage(error.message);
-			}
-
-			//make necessary state updates
-		});
-	};
-
-	const toggleRecord = () => {
-		if (micPermissionStatus === 'denied') {
-			setErrorMessage('Permission to Record Audio has been denied');
-			return;
-		}
-		recorder.toggleRecord((error, stopped) => {
-			if (error) {
-				setErrorMessage(error.message);
-			} else if (stopped) {
-				playerReload();
-				recorderReload();
-			}
-		});
-	};
 
 	useEffect(() => {
 		activateMicrophonePermissionRequest();
