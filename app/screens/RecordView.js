661e35385ca672ab5a4bdb617cec42ffc7843973
alishalawani <alishalawani@gmail.com>
2020-12-08 03:51:52 -0600

    did some error handling for recording audio

--- app/screens/RecordView.js
+++ app/screens/RecordView.js
@@ -10,21 +10,23 @@ import {
 import { Audio } from 'expo-av';
 
 function RecordView({ navigate }) {
-	const [fileName, setFilename] = useState('record.mp4');
+	let recorder = new Audio.Recording();
+	let player = new Audio.Sound();
+	let recording = new Audio.Sound();
 
-	const [player, setPlayer] = useState(new Audio.Sound());
-	const [recorder, setRecorder] = useState(new Audio.Recording());
-	const [recording, setRecording] = useState(null);
+	const [RecordPermissionGranted, setRecordPermissionGranted] = useState(false);
+
 	const [initialStatus, setInitialStatus] = useState({
 		shouldPlay: false,
 		rate: 1.0,
 		shouldCorrectPitch: true,
-		volume: 5.0,
+		volume: 1.0,
 		isMuted: false,
 		isLooping: false,
 	});
 
 	const [buttons, setButtons] = useState('Record');
+
 	function play() {
 		player.getStatusAsync().then((res) => {
 			console.log(res);
@@ -41,28 +43,40 @@ function RecordView({ navigate }) {
 			}
 		});
 	}
-	
+
 	function record() {
-		recorder
-			.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY)
-			.then((res) => {
-				console.log(res);
-				recorder.startAsync();
-				console.log('recording...');
+		const prepareToRecord = () => {
+			recorder
+				.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY)
+				.then((res) => {
+					console.log(res);
+					recorder.startAsync();
+					console.log('recording...');
+				});
+		};
+		if (RecordPermissionGranted) {
+			recorder.getStatusAsync().then((status) => {
+				console.log(status);
+				if (status.canRecord) {
+					prepareToRecord();
+				} else {
+					recorder = new Audio.Recording();
+					prepareToRecord();
+				}
 			});
+		}
 	}
 
 	function playRecording() {
-		recording.getStatusAsync().then((res) => {
-			console.log(res);
-			if (res.isLoaded) {
+		recording.getStatusAsync().then((status) => {
+			console.log(status);
+			if (status.isLoaded) {
 				recording.replayAsync();
+				console.log('replaying recording...');
 			} else {
 				recorder.createNewLoadedSoundAsync(initialStatus).then((res) => {
-					setRecording(res.sound);
-					recording.playAsync();
-					// recording.setVolumeAsync(5)
-					console.log('playing recording');
+					res.sound.playAsync();
+					console.log('playing recording...');
 				});
 			}
 		});
@@ -81,7 +95,6 @@ function RecordView({ navigate }) {
 			allowsRecordingIOS: true,
 			playsInSilentModeIOS: true,
 		});
-		player.setVolumeAsync(5);
 	}, []);
 
 	return (
