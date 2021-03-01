fa9e295ae40d9e0d13a7aa56781bd8e93428297c
alishalawani <alishalawani@gmail.com>
2021-02-28 19:38:31 -0600

    designed the signup page

--- app/screens/SignUpScreen.js
+++ app/screens/SignUpScreen.js
@@ -7,6 +7,8 @@ import {
 	View,
 	TextInput,
 	TouchableOpacity,
+	ImageBackground,
+	Image,
 } from 'react-native';
 
 function SignUpScreen(props) {
@@ -45,15 +47,15 @@ function SignUpScreen(props) {
 			retypePassword: password,
 		});
 	};
-	
+
 	const handleSubmit = () => {
 		const key = 'key';
 		let url = 'http://localhost:8000/users/create_user/' + key;
 		let body = {
 			name: userInfo.name,
 			email: userInfo.email,
-			password: userInfo.password
-		}
+			password: userInfo.password,
+		};
 
 		fetch(url, {
 			method: 'POST',
@@ -68,54 +70,57 @@ function SignUpScreen(props) {
 				//the id would be passed through the navigator to the next
 				console.log(res);
 			});
-
 	};
 
 	return (
 		<SafeAreaView style={styles.container}>
-			<Text style={styles.heading}>Sign Up</Text>
-			<View style={styles.inputBoxContainer}>
+			<View style={styles.topContent}>
+				<Image source={require('../assets/logo-icon.png')} 
+				style={styles.logo} />
+			</View>
+			<View style={styles.bottomContents}>
+				<Text style={styles.heading}>Sign Up</Text>
+				<Text style={styles.subHeading}>
+					{' '}
+					Signup to get started with the app.
+				</Text>
+
 				<TextInput
 					style={styles.inputBox}
 					onChangeText={(text) => handleUsernameChange(text)}
 					value={userInfo.name}
 					placeholder='Username'
-					placeholderTextColor='black'
+					placeholderTextColor='#7c8ca0'
 				/>
-			</View>
 
-			<View style={styles.inputBoxContainer}>
 				<TextInput
 					style={styles.inputBox}
 					onChangeText={(text) => handleEmailChange(text)}
 					value={userInfo.email}
 					placeholder='Email'
-					placeholderTextColor='black'
+					placeholderTextColor='#7c8ca0'
 				/>
-			</View>
 
-			<View style={styles.inputBoxContainer}>
 				<TextInput
 					style={styles.inputBox}
 					onChangeText={(text) => handlePasswordChange(text)}
 					placeholder='Password'
 					value={userInfo.password}
-					placeholderTextColor='black'
+					placeholderTextColor='#7c8ca0'
 				/>
-			</View>
 
-			<View style={styles.inputBoxContainer}>
 				<TextInput
 					style={styles.inputBox}
 					onChangeText={(text) => handleRetypePasswordChange(text)}
 					placeholder='Confirm Password'
 					value={userInfo.password}
-					placeholderTextColor='black'
+					placeholderTextColor='#7c8ca0'
 				/>
+				<TouchableOpacity style={styles.button} onPress={handleSubmit}>
+					<Text style={styles.buttonTitle}>SIGN UP</Text>
+				</TouchableOpacity>
+				<Text style={styles.loginPageNavigator}>Login to my Account</Text>
 			</View>
-			<TouchableOpacity style={styles.button} onPress={handleSubmit}>
-				<Text style={styles.buttonTitle}>Create Account</Text>
-			</TouchableOpacity>
 		</SafeAreaView>
 	);
 }
@@ -126,34 +131,65 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		alignItems: 'center',
-		justifyContent: 'center',
 		width: '100%',
-		backgroundColor: 'white',
-		color: 'white',
+		color: '#ffffff',
+		backgroundColor: '#ffffff',
+	},
+	topContent: {
+		width: '100%',
+		height: '30%',
+		alignSelf: 'flex-start',
+		top: 0,
+		justifyContent: 'center',
+	},
+	logo: {
+		alignSelf: 'center',
+		height: 100,
+		width: 100,
 	},
 	heading: {
-		fontSize: 20,
-		marginBottom: 15,
+		fontSize: 35,
+		marginBottom: 22,
+		alignSelf: 'flex-start',
+		color: '#0a2549',
 	},
-	inputBox: { flex: 1 },
-	inputBoxContainer: {
-		borderBottomWidth: 1,
-		flexDirection: 'row',
-		width: '70%',
-		marginBottom: 25,
+	subHeading: {
+		fontSize: 18,
+		marginBottom: 45,
+		color: '#546780',
+	},
+	bottomContents: {
+		width: '80%',
+		maxWidth: 300,
+		justifyContent: 'center',
+		paddingVertical: 30,
+	},
+	inputBox: {
+		borderColor: '#eff2f7',
+		padding: 12,
+		width: '100%',
+		borderWidth: 1,
+		borderRadius: 10,
+		backgroundColor: '#eff2f7',
+		marginBottom: 19,
 	},
 	button: {
-		backgroundColor: 'dodgerblue',
-		marginTop: 20,
-		marginBottom: 20,
-		padding: 8,
-		width: 200,
-		borderRadius: 25,
+		backgroundColor: '#b4f3b4',
+		marginTop: 15,
+		width: '100%',
+		marginBottom: 25,
+		padding: 12,
+		borderRadius: 10,
 	},
 	buttonTitle: {
 		color: 'white',
 		fontSize: 18,
 		fontWeight: 'bold',
 		alignSelf: 'center',
+	},
+	loginPageNavigator: {
+		color: '#445973',
+		fontSize: 20,
+		alignSelf:'center',
 	},
 });
